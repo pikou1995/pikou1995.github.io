a0f4630af683945f2f0e2c3955dd541a2e9269b5
pikou1995 <434708082@qq.com>
2016-07-28 15:58:10 +0800

    。。。

---
+++ javascripts/renderTitlesNew.js
@@ -0,0 +1,29 @@
+var TITLES_URL = 'data/titles.json';
+var titles = [{"title":"No data!","date":"2016/7/19","src":"500"}];
+
+
+
+window.onload = function(){
+	$.ajax({
+		type : 'GET',
+		url : TITLES_URL,
+		dataType : 'JSON',
+		success : function(data){
+            $('#main_content').html('');
+            renderTitles(data);},
+		error : function(err){
+			alert(err);
+			renderTitles(titles);
+		}
+	});
+};
+
+function renderTitles(titles){
+	for(var src = 0; src < titles.length; src++){
+		var innerHtml = '<a href="details/templateNew.html?No='+src+'"><h3>'+titles[src].title+'<small class="pull-right">'+ titles[src].date +'</small></h3></a>';
+		var item = $('<article></article>');
+		item.html(innerHtml);
+		$('#main_content').append(item);
+	}
+
+}
