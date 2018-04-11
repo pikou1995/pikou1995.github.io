8c6754f472929564db9907bbd4506ec20fdb15cd
pikou <434708082@qq.com>
2018-04-11 16:19:24 +0800

    整理了一下文件. 现在浏览器用ajax不允许使用file://协议, 简单弄了个http server. 可以本地预览.

--- javascripts/renderDetail.js
+++ javascripts/renderDetail.js
@@ -1,50 +1,47 @@
-var ERROR_URL = '../data/404.json';
+var ERROR_URL = '../data/404.json'
 
 
 
-function init(url){
-	$.ajax({
-		type : 'GET',
-		url : url,
-		dataType : 'JSON',
-		success : function(data){renderDetail(data);},
-		error : function(err){
-			init(ERROR_URL);
-		}
-	});
+function init(url) {
+    $.ajax({
+        url: url,
+        dataType: 'JSON',
+        success: function (data) { renderDetail(data) },
+        error: function (err) {
+            init(ERROR_URL)
+        }
+    })
 }
 
-function renderMarkdown(selector, file){
-	$.ajax({
-		type : 'GET',
-		url : '../markdowns/' + GetQueryString('No') + '/' + file,
-		dataType : 'text',
-		success : function(data){
-			$(selector).html(marked(data)).promise().done(function(){
-				$(selector + ' img').attr('class', 'img-responsive');
-			})
-		}.bind(this),
-		error : function(err){
-			$(selector).html('<p style="color: orange;">markdown file not found!</p>');
-		}
-	});
-	
-}
-function renderDetail(detail){
-	var innerHtml = '<h1 class="text-center">'+detail[0].title+'</h1>' + 
-		'<h6 class="text-center text-muted">'+ detail[0].date +'</h6>'+
-		detail[0].content;
-	$('#main_content').html(innerHtml);
+function renderMarkdown(selector, file) {
+    $.ajax({
+        url: '../markdowns/' + getQueryString('id') + '/' + file,
+        dataType: 'text',
+        success: function (data) {
+            $(selector).html(marked(data)).promise().done(function () {
+                $(selector + ' img').attr('class', 'img-responsive')
+            })
+        }.bind(this),
+        error: function (err) {
+            $(selector).html('<p style="color: orange;">markdown file not found!</p>')
+        }
+    })
 }
 
-function GetQueryString(parameter){
-	var reg = new RegExp("(^|&)" + parameter + "=([^&]*)(&|$)");
-	var value = window.location.search.substr(1).match(reg);
-	if(value !== null) return unescape(value[2]);
-	return null;
+function renderDetail(detail) {
+    var innerHtml = '<h1 class="text-center">' + detail[0].title + '</h1>' +
+        '<h6 class="text-center text-muted">' + detail[0].date + '</h6>' +
+        detail[0].content
+    $('#main_content').html(innerHtml)
 }
 
-window.onload = function(){
-	console.log(1);
-	init('../data/' + (GetQueryString('No') || 404) + '.json');
-};
+function getQueryString(parameter) {
+    var reg = new RegExp("(^|&)" + parameter + "=([^&]*)(&|$)")
+    var value = window.location.search.substr(1).match(reg)
+    if (value !== null) return unescape(value[2])
+    return null
+}
+
+window.onload = function () {
+    init('../data/' + (getQueryString('id') || 404) + '.json')
+}
--- markdowns/14/php_git_webhook.md
+++ markdowns/14/php_git_webhook.md
@@ -1,19 +1,19 @@
 > 一个朋友在参考 [一个教程](http://m.aoh.cc/149.html) 时遇到问题, 我帮助他搞了很久, 现在记录一下经验.
 
-> 本文是基于**已经搭建好LAMP环境**的前提下
+> 本文是基于 **已经搭建好LAMP环境** 的前提下
 
 # 一. 文件权限
 
-不管是nginx还是apache服务器,执行php代码的**都不是root**或者你登录的那个账号, centos中是`www`, ubuntu中是`www-data`, 以下统称他们为www.
+不管是nginx还是apache服务器,执行php代码的**都不是root**或者你登录的那个账号, centos中是 `www` , ubuntu中是 `www-data` , 以下统称他们为www.
 
 ## 确认www的home文件夹位置
 
-### 1. 为什么? 
+### 1. 为什么?
 
-当我们配置`sudo -Hu www git config --global user.email`等时配置文件需要放入home文件夹,如果不存在就会出错.
+当我们配置 `sudo -Hu www git config --global user.email` 等时配置文件需要放入home文件夹,如果不存在就会出错.
 
-### 2. 怎么确认? 
- 
+### 2. 怎么确认?
+
 `cat /etc/passwd` 找到类似下面的,home文件夹是倒数第二个':'后面的文件夹, 然后确认文件夹是否存在, 用户和用户组是否属于www.
 
 ```
@@ -43,24 +43,24 @@ www-data:x:33:33:www-data:/var/www:/usr/sbin/nologin
 # chown -R www:www /var/www/test
 ```
 
-> 需要注意的是, 之后请**不要直接使用git命令**,否则文件权限又会发生变化
+> 需要注意的是, 之后请 **不要直接使用git命令** ,否则文件权限又会发生变化
 
 # 二. php配置
 
-一般为了服务器安全, 默认的php.ini会禁用一些function, 比如`exec`, `shell_exec`等. 在这里我也探讨安全问题, 只是单纯的想使用webhook带来的便利.
+一般为了服务器安全, 默认的php.ini会禁用一些function, 比如`exec`, `shell_exec`等. 在这里不探讨安全问题, 只是单纯的想使用webhook带来的便利.
 
 ## 如何确认?
 
-* 可以随便写个phpinfo()的测试php,查找`disable_functions` 和 `Loaded Configuration File` 查看shell_exec有没有被禁用和php.ini的位置
+* 可以随便写个phpinfo()的测试php,查找 `disable_functions` 和 `Loaded Configuration File` 查看shell_exec有没有被禁用和php.ini的位置
 
-* 熟悉自己配置的可以直接看php.ini的`disable_functions`
+* 熟悉自己配置的可以直接看php.ini的 `disable_functions`
 
 ## 如何处理?
 
-* 编辑php.ini, 把`shell_exec` 从`disable_functions`中删除
+* 编辑php.ini, 把 `shell_exec` 从 `disable_functions` 中删除
 * 重启nginx,apache,php-fpm
 
-> nginx尤其要**注意重启php-fpm**, 只重启nginx不会生效
+> nginx尤其要 **注意重启php-fpm** , 只重启nginx不会生效
 
 # 三. php代码
 
