76018616ae8524d93675ae1c39c2172f8a8b6a5a
pikou <434708082@qq.com>
2020-08-19 23:47:24 +0800

    改成koa

--- app.js
+++ app.js
@@ -1,42 +1,10 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const serve = require('koa-static');
+const Koa = require('koa');
+const app = new Koa();
 
-var app = express()
+// $ GET /index.html
+app.use(serve('.'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.listen(3000);
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'))
-})
-
-app.use('/', express.static('.'))
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-})
-
-module.exports = app
+console.log('listening on port 3000');
